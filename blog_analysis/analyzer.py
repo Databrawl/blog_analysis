9326d2da9f2bc0f900980e7a28fc19c53277c699
Serge Mosin <svmosin@gmail.com>
2017-08-24 22:39:08 +0600

    Main module was added support for argument based command line launch. Estimate traffic spider cmd option was added.

--- blog_analysis/analyzer.py
+++ blog_analysis/analyzer.py
@@ -1,4 +1,7 @@
+import argparse
 import csv
+import glob
+import json
 import os
 from operator import itemgetter
 
@@ -6,6 +9,9 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 from scraping.spiders.blogs import BlogsSpider
+from scraping.spiders.traffic import TrafficSpider
+
+settings = get_project_settings()
 
 
 def get_top_languages(n, file_name):
@@ -30,15 +36,41 @@ def get_top_languages(n, file_name):
 
 def get_top_blogs():
     """
-    Analyze connection between post frequency and visitors
-    :return:
+    Get URLs of most popular blog posts for most popular programming languages
+    on GitHub.
     """
-    settings = get_project_settings()
     languages = get_top_languages(30, settings['LANGUAGES_DATA'])
     process = CrawlerProcess(settings)
     process.crawl(BlogsSpider, languages)
     process.start()  # the script will block here until the crawling is done
 
 
+def get_latest_file(directory):
+    list_of_files = glob.glob(os.path.join(directory, '*'))
+    latest_file = max(list_of_files, key=os.path.getctime)
+    return latest_file
+
+
+def estimate_traffic():
+    """
+    Analyze traffic of the scraped blogs.
+    """
+    process = CrawlerProcess(settings)
+    blogs_file = get_latest_file(settings['BLOGS_FEED_DIR'])
+    with open(blogs_file) as f:
+        blogs = json.load(f)
+    process.crawl(TrafficSpider, blogs)
+    process.start()  # the script will block here until the crawling is done
+
+
 if __name__ == '__main__':
-    get_top_blogs()
+    desc = 'Analyze and compare programming blogs traffic.'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('spider', choices=['blogs', 'traffic'],
+                        help='Scrapy spider name to run.')
+
+    args = parser.parse_args()
+    if args.spider == 'blogs':
+        get_top_blogs()
+    elif args.spider == 'traffic':
+        estimate_traffic()
