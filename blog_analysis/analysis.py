96c5725e8501ebf485229f6936eb1811b91ea0c9
Serge Mosin <svmosin@gmail.com>
2017-08-31 23:06:44 +0600

    Analysis function return format was adjusted to suit bokeh.

--- blog_analysis/analysis.py
+++ blog_analysis/analysis.py
@@ -7,8 +7,11 @@ def get_languages_popularity(data):
     Count total daily views for all languages and sort in descending order.
     """
     query_sorted_data = sorted(data, key=itemgetter('query'))
+    result = {'languages': [], 'views': []}
     for k, group in groupby(query_sorted_data, key=itemgetter('query')):
         group = list(group)
         daily_page_views = map(lambda r: int(r['daily_page_views']), group)
         total_page_views = sum(daily_page_views)
-        yield (group[0]['query'], total_page_views)
+        result['languages'].append(group[0]['query'])
+        result['views'].append(total_page_views)
+    return result
