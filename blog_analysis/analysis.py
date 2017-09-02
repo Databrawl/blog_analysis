e533c78b39f19fed61fadda28e75450b4d22b5ea
Serge Mosin <svmosin@gmail.com>
2017-09-02 16:11:37 +0600

    Languages popularity analysis results are now being sorted.

--- blog_analysis/analysis.py
+++ blog_analysis/analysis.py
@@ -8,10 +8,14 @@ def get_languages_popularity(data):
     """
     query_sorted_data = sorted(data, key=itemgetter('query'))
     result = {'languages': [], 'views': []}
+    popularity = []
     for k, group in groupby(query_sorted_data, key=itemgetter('query')):
         group = list(group)
         daily_page_views = map(lambda r: int(r['daily_page_views']), group)
         total_page_views = sum(daily_page_views)
-        result['languages'].append(group[0]['query'])
-        result['views'].append(total_page_views)
+        popularity.append((group[0]['query'], total_page_views))
+    sorted_popularity = sorted(popularity, key=itemgetter(1), reverse=True)
+    languages, views = zip(*sorted_popularity)
+    result['languages'] = languages
+    result['views'] = views
     return result
