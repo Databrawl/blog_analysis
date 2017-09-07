3b73756ad21a68c63676369ef49b68b01628df57
Serge Mosin <svmosin@gmail.com>
2017-09-07 15:55:14 +0600

    Ranking/views retrieval analysis function was implemented.

--- blog_analysis/analysis.py
+++ blog_analysis/analysis.py
@@ -19,3 +19,22 @@ def get_languages_popularity(data):
     result['languages'] = languages
     result['views'] = views
     return result
+
+
+def get_ranking_and_views(data):
+    """
+    Extract ranking and views information and structure it by languages.
+
+    :param data: scraped data
+    :return: dict of language:data key/value pairs
+    """
+    query_sorted_data = sorted(data, key=itemgetter('query'))
+    result = {}
+    for k, group in groupby(query_sorted_data, key=itemgetter('query')):
+        group = list(group)
+        ranks = map(lambda r: int(r['rank']), group)
+        daily_page_views = map(lambda r: int(r['daily_page_views']), group)
+        # ranks_views_data = list(zip(ranks, daily_page_views))
+        ranks_views_data = (ranks, daily_page_views)
+        result[group[0]['query']] = ranks_views_data
+    return result
